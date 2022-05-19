e0161a1c1d51e98d2a8c52da66f896a81875fabf
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2022-05-19 19:17:46 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+
+const DEBOUNCE_DELAY = 300;
+
+
+
+
+Notiflix.Notify.failure("Oops, there is no country with that name");
+Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
